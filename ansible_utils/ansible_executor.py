9a5a91a10db92d9a69dafa5436eb358cb66077c9
DanyalTaghipor <47154952+DanyalTaghipor@users.noreply.github.com>
2024-06-05 17:51:24 +0000

    feat(update): veriosn

--- ansible_utils/ansible_executor.py
+++ ansible_utils/ansible_executor.py
@@ -19,7 +19,6 @@ def setup_and_run_playbook(nickname, play_source):
         become_method=None,
         become_user=None,
         check=False,
-        cache=True,
         diff=False,
         remote_user=None,
         verbosity=3
@@ -41,13 +40,14 @@ def setup_and_run_playbook(nickname, play_source):
         if tqm is not None:
             tqm.cleanup()
 
-def install_tool(nickname, role_name, version):
+def install_tool(nicknames, role_name, version):
+    hosts_str = ','.join(nicknames)
     play_source = dict(
         name=f"Install {role_name}",
-        hosts=nickname,
+        hosts=hosts_str,
         gather_facts='no',
         tasks=[
             dict(name=f"Install {role_name}", import_role=dict(name=role_name))
         ]
     )
-    return setup_and_run_playbook(nickname, play_source)
+    return setup_and_run_playbook(hosts_str, play_source)
